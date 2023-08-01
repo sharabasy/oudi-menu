9b40d3cf3af937e6afb83268b884aed3c2c5c021
ZiadAssal <zead142684@eng.psu.edu.eg>
2023-08-01 18:34:04 +0300

    Project Gallary

--- src/components/portfolio/PortfolioGallery.js
+++ src/components/portfolio/PortfolioGallery.js
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import style from "./portfolioGallery.module.scss";
 import portfolioItems from "../../data/portfolioItems.js";
 
 const PortfolioGallery = () => {
+    const [selectedProject, setSelectedProject] = useState(null);
+
+    const handleProjectHover = (projectId) => {
+        setSelectedProject(projectId);
+    };
+
+    const handleProjectHoverLeave = () => {
+        setSelectedProject(null);
+    };
+
     return (
-        <div className={`flex-row-center flex-wrap ${style["portfolio-gallery"]}`}>
-            {portfolioItems.map((project) => (
+        <div className={style["portfolio-gallery"]}>
+            {portfolioItems.map((project, index) => (
                 <motion.div
                     key={project.id}
                     className={style["portfolio-gallery--item"]}
                     initial={{ opacity: 0, y: 20 }}
                     animate={{ opacity: 1, y: 0 }}
-                    whileHover={{ scale: 1.05, transition: { duration: 0.2 } }} // Add scale animation on hover
                     transition={{ duration: 0.5, ease: "easeOut" }}
+                    onMouseEnter={() => handleProjectHover(project.id)}
+                    onMouseLeave={handleProjectHoverLeave}
                 >
                     <motion.img
                         src={project.coverImage}
@@ -22,15 +33,28 @@ const PortfolioGallery = () => {
                         animate={{ opacity: 1 }}
                         transition={{ duration: 0.5, delay: 0.2 }}
                     />
-                    <div className={`size-24px inter font-bold margin-5px-V`}>{project.title}</div>
-                    <div className={`size-16px`}>{project.description}</div>
-                    <div className={`flex-row-center ${style["technology-tags"]}`}>
-                        {project.technologies.map((technology) => (
-                            <div key={technology} className={`radius-20px ${style["technology-tag"]}`}>
-                                {technology}
+
+                    {selectedProject === project.id && (
+                        <motion.div
+                            className={`flex-col-center ${style["portfolio-gallery--item-overlay"]}`}
+                            initial={{ opacity: 0, y: -10 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 0.2 }}
+                        >
+                            <div className={`size-24px inter font-bold margin-5px-V`}> {project.title} </div>
+                            <div className={`size-16px`}>{project.description}</div>
+                            <div className={`flex-row-center ${style["technology-tags"]}`}>
+                                {project.technologies.map((technology) => (
+                                    <div
+                                        key={technology}
+                                        className={`radius-20px ${style["technology-tag"]}`}
+                                    >
+                                        {technology}
+                                    </div>
+                                ))}
                             </div>
-                        ))}
-                    </div>
+                        </motion.div>
+                    )}
                 </motion.div>
             ))}
         </div>

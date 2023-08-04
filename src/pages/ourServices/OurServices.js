f1249cb99bc06b3d04f7ed1b1f0ce44a180d17b2
Abdelrahman El Sharabasy <boodysharabas@gmail.com>
2023-08-04 20:35:47 +0300

    responsive WhoAreWe & animation ourServices

--- src/pages/ourServices/OurServices.js
+++ src/pages/ourServices/OurServices.js
@@ -1,67 +1,83 @@
 import React from "react";
 import ScrollAnimation from 'react-animate-on-scroll';
+import ReactImg from '../../assets/images/react.png';
+import Flutter from '../../assets/images/flutter.png';
+import NodeJS from '../../assets/images/node-js.png';
+import Kubernetes from '../../assets/images/Kubernetes.png';
 
 import style from './ourServices.module.scss';
 
 const OurServices = () => {
     return (
         <div className={`${style['page']} full-width flex-col-top-start`}>
-            <h1 className="size-34px pt-sans">Our Services</h1>
-            <div className={`${style['services']} full-width shadow-5px radius-10px`}>
-                <ScrollAnimation duration={2} animateIn="fadeIn" animateOnce={false} offset={300} animatePreScroll={false} className={`${style['service']} flex-row-between full-width`}>
-                    <div className={`${style['piece']} ${style['piece--head']} dark-blue-bg radius-10px flex-row-center`}>
+            <h1 className="size-34px pt-sans white text-shadow">Our Services</h1>
+            <div className={`${style['services']} full-width shadow-5px white-bg radius-10px`}>
+                <div className={`${style['service']} flex-row-between full-width`}>
+                    <ScrollAnimation duration={2} animateIn="fadeIn" offset={250} animatePreScroll={false} initiallyVisible className={`${style['piece']} ${style['piece--head']} dark-blue-bg radius-10px flex-row-center`}>
                         <div className={`${style['piece--head--right']} dark-blue-bg`}></div>
                         <div className={`${style['piece--head--bottom']} dark-blue-bg`}></div>
-                        <div className={`${style['piece--cont']} white inter size-24px`}>React</div>
-                    </div>
-                    <div className={`${style['piece']} ${style['piece--description']} baby-blue-bg radius-10px flex-row-center`}>
+                        <div className={`${style['piece--cont']} white inter size-24px flex-col-center`}>
+                            <img src={ReactImg} alt="React" className={`${style['piece--cont--img']}`} />
+                            React.js
+                        </div>
+                    </ScrollAnimation>
+                    <ScrollAnimation duration={2} animateIn="fadeIn" offset={350} animatePreScroll={false} className={`${style['piece']} ${style['piece--description']} baby-blue-bg radius-10px flex-row-center`}>
                         <div className={`${style['piece--description--bottom']} baby-blue-bg ${style['r-side']}`}></div>
                         <div className={`${style['piece--description--left']} baby-blue-bg`}></div>
                         <div className={`${style['piece--description--top--responsive']} baby-blue-bg`}></div>
                         <div className={`${style['piece--cont']} white inter size-12px`}>Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque accusantium quod minus obcaecati fugit, quae maiores minima pariatur reprehenderit ab consectetur magni aperiam eveniet inventore cupiditate natus distinctio, est consequuntur.</div>
-                    </div>
-                </ScrollAnimation>
-                <ScrollAnimation duration={2} animateIn="fadeIn" animateOnce={false} offset={300} animatePreScroll={false} className={`${style['service']} ${style['reverse']} flex-row-between full-width`}>
-                    <div className={`${style['piece']} ${style['piece--description']} baby-blue-bg radius-10px flex-row-center`}>
+                    </ScrollAnimation>
+                </div>
+                <div className={`${style['service']} ${style['reverse']} flex-row-between full-width`}>
+                    <ScrollAnimation duration={2} animateIn="fadeIn" offset={350} animatePreScroll={false} className={`${style['piece']} ${style['piece--description']} baby-blue-bg radius-10px flex-row-center`}>
                         <div className={`${style['piece--description--top']} baby-blue-bg`}></div>
                         <div className={`${style['piece--description--right']} baby-blue-bg`}></div>
                         <div className={`${style['piece--description--bottom']} baby-blue-bg`}></div>
                         <div className={`${style['piece--cont']} white inter size-12px`}>Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque accusantium quod minus obcaecati fugit, quae maiores minima pariatur reprehenderit ab consectetur magni aperiam eveniet inventore cupiditate natus distinctio, est consequuntur.</div>
-                    </div>
-                    <div className={`${style['piece']} ${style['piece--head']} dark-blue-bg radius-10px flex-row-center`}>
+                    </ScrollAnimation>
+                    <ScrollAnimation duration={2} animateIn="fadeIn" offset={250} animatePreScroll={false} className={`${style['piece']} ${style['piece--head']} dark-blue-bg radius-10px flex-row-center`}>
                         <div className={`${style['piece--head--top']} dark-blue-bg ${style['r-side']}`}></div>
                         <div className={`${style['piece--head--left']} dark-blue-bg`}></div>
                         <div className={`${`${style['piece--head--bottom']} dark-blue-bg`} ${style['r-side']}`}></div>
-                        <div className={`${style['piece--cont']} white inter size-24px`}>Flutter</div>
-                    </div>
-                </ScrollAnimation>
-                <ScrollAnimation duration={2} animateIn="fadeIn" animateOnce={false} offset={300} animatePreScroll={false} className={`${style['service']} flex-row-between full-width`}>
-                    <div className={`${style['piece']} ${style['piece--head']} dark-blue-bg radius-10px flex-row-center`}>
+                        <div className={`${style['piece--cont']} white inter size-24px`}>
+                            <img src={Flutter} alt="Flutter" className={`${style['piece--cont--img']}`} />
+                            Flutter
+                        </div>
+                    </ScrollAnimation>
+                </div>
+                <div className={`${style['service']} flex-row-between full-width`}>
+                    <ScrollAnimation duration={2} animateIn="fadeIn" offset={250} animatePreScroll={false} className={`${style['piece']} ${style['piece--head']} dark-blue-bg radius-10px flex-row-center`}>
                         <div className={`${style['piece--head--top']} dark-blue-bg`}></div>
                         <div className={`${style['piece--head--right']} dark-blue-bg`}></div>
                         <div className={`${style['piece--head--bottom']} dark-blue-bg`}></div>
-                        <div className={`${style['piece--cont']} white inter size-24px`}>NodeJS</div>
-                    </div>
-                    <div className={`${style['piece']} ${style['piece--description']} baby-blue-bg radius-10px flex-row-center`}>
+                        <div className={`${style['piece--cont']} white inter size-24px`}>
+                            <img src={NodeJS} alt="NodeJS" className={`${style['piece--cont--img']}`} />
+                            NodeJS
+                        </div>
+                    </ScrollAnimation>
+                    <ScrollAnimation duration={2} animateIn="fadeIn" offset={350} animatePreScroll={false} className={`${style['piece']} ${style['piece--description']} baby-blue-bg radius-10px flex-row-center`}>
                         <div className={`${style['piece--description--top']} baby-blue-bg ${style['r-side']}`}></div>
                         <div className={`${style['piece--description--left']} baby-blue-bg`}></div>
                         <div className={`${style['piece--description--bottom']} baby-blue-bg ${style['r-side']}`}></div>
                         <div className={`${style['piece--cont']} white inter size-12px`}>Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque accusantium quod minus obcaecati fugit, quae maiores minima pariatur reprehenderit ab consectetur magni aperiam eveniet inventore cupiditate natus distinctio, est consequuntur.</div>
-                    </div>
-                </ScrollAnimation>
-                <ScrollAnimation duration={2} animateIn="fadeIn" animateOnce={false} offset={300} animatePreScroll={false} className={`${style['service']} ${style['reverse']} flex-row-between full-width`}>
-                    <div className={`${style['piece']} ${style['piece--description']} baby-blue-bg radius-10px flex-row-center`}>
+                    </ScrollAnimation>
+                </div>
+                <div className={`${style['service']} ${style['reverse']} flex-row-between full-width`}>
+                    <ScrollAnimation duration={2} animateIn="fadeIn" offset={350} animatePreScroll={false} className={`${style['piece']} ${style['piece--description']} baby-blue-bg radius-10px flex-row-center`}>
                         <div className={`${style['piece--description--top']} baby-blue-bg`}></div>
                         <div className={`${style['piece--description--right']} baby-blue-bg`}></div>
                         <div className={`${style['piece--cont']} white inter size-12px`}>Description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque accusantium quod minus obcaecati fugit, quae maiores minima pariatur reprehenderit ab consectetur magni aperiam eveniet inventore cupiditate natus distinctio, est consequuntur.</div>
-                    </div>
-                    <div className={`${style['piece']} ${style['piece--head']} dark-blue-bg radius-10px flex-row-center`}>
+                    </ScrollAnimation>
+                    <ScrollAnimation duration={2} animateIn="fadeIn" offset={250} animatePreScroll={false} className={`${style['piece']} ${style['piece--head']} dark-blue-bg radius-10px flex-row-center`}>
                         <div className={`${style['piece--head--top']} dark-blue-bg ${style['r-side']}`}></div>
                         <div className={`${style['piece--head--left']} dark-blue-bg`}></div>
                         <div className={`${style['piece--head--bottom--responsive']} dark-blue-bg`}></div>
-                        <div className={`${style['piece--cont']} white inter size-24px`}>React Native</div>
-                    </div>
-                </ScrollAnimation>
+                        <div className={`${style['piece--cont']} white inter size-24px`}>
+                            <img src={Kubernetes} alt="Kubernetes" className={`${style['piece--cont--img']}`} />
+                            Kubernetes
+                        </div>
+                    </ScrollAnimation>
+                </div>
             </div>
         </div>
     );
--- src/pages/whoAreWe/WhoAreWe.js
+++ src/pages/whoAreWe/WhoAreWe.js
@@ -14,14 +14,14 @@ const WhoAreWe = () => {
                 We are a team of passionate and skilled developers, dedicated to creating innovative solutions and pushing the boundaries of technology. With expertise in various domains, we bring a diverse set of skills to the table, allowing us to tackle complex projects and deliver exceptional results.
             </p>
             <div className={style["divider"]} /> {/* Add a horizontal rule (line) */}
-            <div className={`${style["mission"]} flex-row-center`}>
+            <div className={`${style["mission"]} flex-row-between2col`}>
                 <img className={style['img']} src={ourMission} alt="Our Mission" />
                 <p className={`${style["text"]} space-none size-20px inter margin-12px-H`}>
                     Our mission is to build cutting-edge applications and systems that not only meet but exceed the expectations of our clients. We strive to create user-friendly and seamless experiences that leave a lasting impact on users and businesses alike.
                 </p>
             </div>
             <div className={style["divider"]} /> {/* Add a horizontal rule (line) */}
-            <div className={`${style["vision"]} flex-row-center`}>
+            <div className={`${style["vision"]} flex-row-between2col`}>
                 <p className={`${style["text"]} space-none size-20px inter margin-12px-H`}>
                     We envision a future where technology knows no bounds. We strive to create a world where seamless user experiences, intelligent automation, and intuitive AI systems become an integral part of everyday life. Our vision is to lead the way in shaping the digital landscape, making it accessible and beneficial to all.
                 </p>
@@ -29,25 +29,25 @@ const WhoAreWe = () => {
             </div>
             <div className={style["divider"]} /> {/* Add a horizontal rule (line) */}
             <h1 className="size-34px pt-sans">What Sets Us Apart ?</h1>
-            <div className="flex-row-top-start full-width">
+            <div className="flex-row-top-start2col full-width">
                 <h3 className="margin-4px-V size-24px pt-sans">1- Diverse Expertise: </h3>
                 <p className={`${style["text"]} space-none size-18px inter margin-12px-H`}>
                     Our team comprises specialists in frontend development, backend development, Flutter, and artificial intelligence. This diverse skill set enables us to cover a wide range of technologies and platforms, giving us a competitive edge in the industry.
                 </p>
             </div>
-            <div className="flex-row-top-start full-width">
+            <div className="flex-row-top-start2col full-width">
                 <h3 className="margin-4px-V size-24px pt-sans">2- Collaborative Approach: </h3>
                 <p className={`${style["text"]} space-none size-18px inter margin-12px-H`}>
                     Collaboration is at the heart of our work culture. We believe that the best solutions emerge when different perspectives come together, and our team members actively contribute their unique insights to each project.
                 </p>
             </div>
-            <div className="flex-row-top-start full-width">
+            <div className="flex-row-top-start2col full-width">
                 <h3 className="margin-4px-V size-24px pt-sans">3- Innovation-Driven: </h3>
                 <p className={`${style["text"]} space-none size-18px inter margin-12px-H`}>
                     We thrive on challenges and are always eager to explore new technologies and approaches. Our innovative mindset allows us to continuously improve our skills and deliver creative solutions.
                 </p>
             </div>
-            <div className="flex-row-top-start full-width">
+            <div className="flex-row-top-start2col full-width">
                 <h3 className="margin-4px-V size-24px pt-sans">4- Client-Centric Focus: </h3>
                 <p className={`${style["text"]} space-none size-18px inter margin-12px-H`}>
                     Our clients are at the core of everything we do. Understanding their needs, goals, and vision is paramount, and we tailor our solutions to meet their specific requirements.
